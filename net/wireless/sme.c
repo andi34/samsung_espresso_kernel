e06c0fde0422758d809f53129f5e4081e018543e
Andreas Blaesius <skate4life@gmx.de>
2016-03-15 19:21:31 +0100

    Apply some updates on net / wireless
    - really needed?
    - ToDo: check what's upstream added on 3.0.101 kernel or remove this stuff

--- net/wireless/sme.c
+++ net/wireless/sme.c
@@ -550,6 +550,7 @@ void __cfg80211_roamed(struct wireless_dev *wdev,
 #ifdef CONFIG_CFG80211_WEXT
 	union iwreq_data wrqu;
 #endif
+
 	ASSERT_WDEV_LOCK(wdev);
 
 	if (WARN_ON(wdev->iftype != NL80211_IFTYPE_STATION &&
