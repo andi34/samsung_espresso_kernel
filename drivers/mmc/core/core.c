d2c8c4f5134b8cc2e44c097d60dac0c565575966
Andreas Blaesius <skate4life@gmx.de>
2016-03-15 22:32:22 +0100

    "commenting this function because we r not using it currenlty"

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1792,6 +1792,8 @@ int mmc_set_blocklen(struct mmc_card *card, unsigned int blocklen)
 }
 EXPORT_SYMBOL(mmc_set_blocklen);
 
+/* commenting this function because we r not using it currently */
+#if 0
 static void mmc_hw_reset_for_init(struct mmc_host *host)
 {
 	if (!(host->caps & MMC_CAP_HW_RESET) || !host->ops->hw_reset)
@@ -1800,6 +1802,7 @@ static void mmc_hw_reset_for_init(struct mmc_host *host)
 	host->ops->hw_reset(host);
 	mmc_host_clk_release(host);
 }
+#endif
 
 int mmc_can_reset(struct mmc_card *card)
 {
